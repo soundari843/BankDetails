e2aad62d0efe6479fe6e4e7629b4e6150153ddc3
soundari843 <soundari843@gmail.com>
2025-07-06 21:51:44 +0530

    new code added

---
+++ src/org/bank/details/RbiBank.java
@@ -0,0 +1,22 @@
+package org.bank.details;
+
+public class RbiBank extends StateBank{
+	private void fixed() {
+		System.out.println("fixed:20%");
+	}
+	@Override
+	
+	public void savings() {
+		super.savings();
+		System.out.println("\tsavings:10%\n");
+	}
+	public static void main(String[] args) {
+		RbiBank r = new RbiBank();
+		r.fixed();
+		r.savings();
+		r.deposite();
+	}
+		
+	
+
+}
---
+++ src/org/bank/details/StateBank.java
@@ -0,0 +1,16 @@
+package org.bank.details;
+
+//Method Overriding
+
+public class StateBank {
+	public void savings() {
+		System.out.println("savings:5%");
+		
+
+	}
+	public void deposite() {
+		System.out.println("deposite:15%");
+
+	}
+
+}
